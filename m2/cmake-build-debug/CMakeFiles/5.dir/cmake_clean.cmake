80f96c0dd14e5039c8ac3026adae8ad5778bc583
rayray2002 <rayray2002.huang@gmail.com>
2020-12-02 15:34:32 +0800

    add m2

---
+++ m2/cmake-build-debug/CMakeFiles/5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "5"
+  "5.pdb"
+  "CMakeFiles/5.dir/5.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
